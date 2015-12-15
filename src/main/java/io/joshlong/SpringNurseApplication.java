a6a2b1e475298a599b78f66e8de570050814a2e4
Josh Long <josh@joshlong.com>
2015-12-15 05:38:06 -0800

    initial commit

---
+++ src/main/java/io/joshlong/SpringNurseApplication.java
@@ -0,0 +1,12 @@
+package io.joshlong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNurseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNurseApplication.class, args);
+    }
+}
---
+++ src/test/java/io/joshlong/SpringNurseApplicationTests.java
@@ -0,0 +1,16 @@
+package io.joshlong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringNurseApplication.class)
+public class SpringNurseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
